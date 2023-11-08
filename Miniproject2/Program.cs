961fe1d4add65a1d1188b7e5a6c0faa63c20b318
MAMADI KABA <MAMADI KABA@DESKTOP-A2DNMVJ>
2023-11-08 11:34:29 +0100

    Ajoutez des fichiers projet.

---
+++ Miniproject2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+class Product
+{
+    public Product(string category, string productName, int price)
+    {
+        Category = category;
+        ProductName = productName; 
+        Price = price;
+    }
+    public string Category { get; set; }
+    public string ProductName { get; set; }
+    public int Price { get; set; }
+}
